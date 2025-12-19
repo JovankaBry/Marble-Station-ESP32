541b533bf3c7c88bf97c27e74a474dcf5ca8a748
JovankaBry <bryan.sahetapy@gmail.com>
2025-12-19 22:33:13 +0100

    Working display touchscreen

--- code/draft/3.5Inch Screen Test/src/main.cpp
+++ code/draft/3.5Inch Screen Test/src/main.cpp
@@ -1,19 +1,29 @@
 #include <Arduino.h>
 #include <TFT_eSPI.h>
 
-TFT_eSPI tft = TFT_eSPI();
+TFT_eSPI tft;
+uint16_t x, y;
+
+uint16_t calData[5] = { 275, 3564, 287, 3513, 7};
 
 void setup() {
+  Serial.begin(115200);
 
   tft.init();
   tft.setRotation(1);
+  tft.setTouch(calData);
+
   tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_WHITE);
-  tft.setTextSize(3);
-  tft.setCursor(40, 100);
-  tft.println("Hello ESP32");
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.setTextSize(2);
+  tft.setCursor(20, 20);
+  tft.println("Touch the screen");
 }
 
-void loop(){
-
+void loop() {
+  if (tft.getTouch(&x, &y)) {
+    Serial.printf("x=%u y=%u\n", x, y);
+    tft.fillCircle(x, y, 4, TFT_RED);
+    delay(1);
+  }
 }
