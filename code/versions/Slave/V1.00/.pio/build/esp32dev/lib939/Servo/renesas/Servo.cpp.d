4b6a8a3531dfe3b4f3d5117346a71677cc7f6714
Bryan Sahetapy <bryan.sahetapy@gmail.com>
2026-02-05 21:52:45 +0100

    Added Slave V1.00

---
+++ code/versions/Slave/V1.00/.pio/build/esp32dev/lib939/Servo/renesas/Servo.cpp.d
@@ -0,0 +1,2 @@
+.pio/build/esp32dev/lib939/Servo/renesas/Servo.cpp.o: \
+ .pio/libdeps/esp32dev/Servo/src/renesas/Servo.cpp
---
+++ code/versions/Slave/V1.00/.pio/build/esp32dev/lib939/Servo/xmc/Servo.cpp.d
@@ -0,0 +1,2 @@
+.pio/build/esp32dev/lib939/Servo/xmc/Servo.cpp.o: \
+ .pio/libdeps/esp32dev/Servo/src/xmc/Servo.cpp
