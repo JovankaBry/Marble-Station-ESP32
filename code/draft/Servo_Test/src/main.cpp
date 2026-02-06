6ff340b953bc5fbcd29f2f3fe8e53f6fc3101f2e
JovankaBry <bryan.sahetapy@gmail.com>
2026-02-06 18:56:18 +0100

    Added Door dispenser and sevrvo test

---
+++ code/draft/Servo_Test/src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#include <ESP32Servo.h>
+
+Servo myServo;
+
+void setup() {
+  Serial.begin(115200);
+
+  myServo.attach(13);   // Servo signal on GPIO 13
+  Serial.println("Servo ready");
+}
+
+void loop() {
+  myServo.write(150);    // move to 90°
+  delay(500);
+
+  myServo.write(90);    // move to 90°
+  delay(500);
+}
