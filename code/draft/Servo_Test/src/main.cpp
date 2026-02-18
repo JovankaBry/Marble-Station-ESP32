c37c727ff6ec4c7e0a43c95d6f1ccb90f7cecdba
JovankaBry <bryan.sahetapy@gmail.com>
2026-02-18 17:44:39 +0100

    updated shapr parts

--- code/draft/Servo_Test/src/main.cpp
+++ code/draft/Servo_Test/src/main.cpp
@@ -13,14 +13,15 @@ void setup() {
 
 void drop_ball(){
   
-  myServo.write(100); 
+  myServo.write(110); // Close / Drop Ball
   delay(2000);
-  
-  myServo.write(15); //Open
+
+  myServo.write(15);
   delay(300);
 
-  myServo.write(50); 
-  delay(2000);
+  myServo.write(0);
+  delay(300);
+
 }
 
 void loop() {
