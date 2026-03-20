e6da77abce00c50ce9f6815738509db2cbf07e18
Bryan Sahetapy <bryan.sahetapy@gmail.com>
2026-03-20 18:22:07 +0100

    Added Slave V2.00 for new Station

---
+++ code/versions/Slave/V2.00/include/servo.h
@@ -0,0 +1,6 @@
+#pragma once
+
+void setupServo();
+void servo_1();
+void servo_2();
+void servo_3();
---
+++ code/versions/Slave/V2.00/src/servo.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+#include <ESP32Servo.h>
+#include "servo.h"
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+
+void setupServo() {
+    servo1.attach(12); // Attach the servo to GPIO pin 12
+    servo2.attach(13); // Attach the servo to GPIO pin 14
+    servo3.attach(14); // Attach the servo to GPIO pin 27
+
+    Serial.println("Servos Ready!");
+}
+
+void servo_1(){
+    servo1.write(90);
+    delay(3000);
+    servo1.write(180);
+    delay(3000);
+}
+
+void servo_2(){
+    servo2.write(50);
+    delay(3000);
+    servo2.write(140);
+    delay(3000);
+}
+
+void servo_3(){
+    servo3.write(50);
+    delay(3000);
+    servo3.write(140);
+    delay(3000);
+}
